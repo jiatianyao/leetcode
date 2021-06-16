9948516705b55c54f26427804c9b92029c6c8438
jiatianyao <5lovezhm@163.com>
2021-06-17 07:17:03 +0800

    深搜广搜

---
+++ src/main/java/深搜广搜/l010_solveNQueens_51.java
@@ -0,0 +1,33 @@
+package 深搜广搜;
+
+import java.util.List;
+
+/**
+ * 51. N 皇后
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+ *
+ * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * 解释：如上图所示，4 皇后问题存在两个不同的解法。
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：[["Q"]]
+ *
+ * https://leetcode-cn.com/problems/n-queens/
+ */
+public class l010_solveNQueens_51 {
+
+    public List<List<String>> solveNQueens(int n) {
+        return null;
+    }
+}
