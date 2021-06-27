9af7d6b72cf73fdb5b6abaf0312063cf43d51f68
jiatianyao <5lovezhm@163.com>
2021-06-27 11:25:15 +0800

    深搜广搜

--- src/main/java/深搜广搜/l009_offer_13.java
+++ src/main/java/深搜广搜/l009_offer_13.java
@@ -1,5 +1,7 @@
 package 深搜广搜;
 
+import java.util.LinkedList;
+
 /**
  * 剑指 Offer 13. 机器人的运动范围
  * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
@@ -18,7 +20,40 @@ package 深搜广搜;
  * https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/
  */
 public class l009_offer_13 {
+    int[] dirX = {0,1};
+    int[] dirY = {1,0};
     public int movingCount(int m, int n, int k) {
-        return 0;
+        LinkedList<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        queue.add(new int[]{0,0});
+        int result = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] poll = queue.poll();
+                int x = poll[0];
+                int y = poll[1];
+                for (int j = 0; j < 2; j++) {
+                    int xNew = x + dirX[j];
+                    int yNew = y + dirY[j];
+                    if(xNew >=0 && xNew < m && yNew >=0 && yNew < n && get(xNew) + get(yNew) <=k
+                            && !visited[xNew][yNew]){
+                        result++;
+                        visited[xNew][yNew] = true;
+                        queue.add(new int[]{xNew,yNew});
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private int get(int x) {
+        int result = 0;
+        while(x!=0){
+            result += x % 10;
+            x /= 10;
+        }
+        return result;
     }
 }
--- src/main/java/深搜广搜/l010_solveNQueens_51.java
+++ src/main/java/深搜广搜/l010_solveNQueens_51.java
@@ -1,5 +1,10 @@
 package 深搜广搜;
 
+import exam.A;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -28,6 +33,72 @@ import java.util.List;
 public class l010_solveNQueens_51 {
 
     public List<List<String>> solveNQueens(int n) {
-        return null;
+        List<List<String>> result = new ArrayList<>();
+        if(n==1){
+            List<String> child = new ArrayList<>();
+            child.add("Q");
+            result.add(child);
+            return result;
+        }
+        int[] visited = new int[n];
+        Arrays.fill(visited,-1);
+        bfs(visited,result,0);
+        return result;
+    }
+
+    public void bfs(int[] visited,List<List<String>> result,int row){
+        if(row == visited.length){
+            setList(result,visited);
+            return;
+        }
+        for (int j = 0; j < visited.length; j++) {
+            if(!isOk(visited,row,j)){
+                visited[row] = j;
+                bfs(visited,result,row+1);
+                visited[row] = -1;
+            }
+        }
+    }
+
+    private void setList(List<List<String>> result, int[] visited) {
+        int n = visited.length;
+        List<String> list = new ArrayList<>();
+        for (int i : visited) {
+            String s = setS(n, i);
+            list.add(s);
+        }
+        result.add(list);
+    }
+
+
+    public String setS(int n,int index){
+        String res = "";
+        while (n>0){
+            if(index+1 == n){
+                res = "Q"+res;
+            }else{
+                res = "."+res;
+            }
+            n--;
+
+        }
+        return res;
+    }
+
+    public boolean isOk(int[] visit,int x2,int y2){
+        for (int x = 0; x < visit.length; x++) {
+            int y = visit[x];
+            if(y==y2 ) return true;
+            if(y==-1) continue;
+            if(Math.abs(x2-x) == Math.abs(y2-y)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        l010_solveNQueens_51 demo = new l010_solveNQueens_51();
+        demo.solveNQueens(4);
     }
 }
