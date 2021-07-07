b72eefce8107ec40369e32aea0b376753960e5c9
jiatianyao <5lovezhm@163.com>
2021-07-07 21:22:11 +0800

    双遍历

---
+++ src/main/java/深搜广搜/l017_candy_135.java
@@ -0,0 +1,52 @@
+package 深搜广搜;
+
+import java.util.Arrays;
+
+/**
+ * 135. 分发糖果
+ * 老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，
+ * 老师会根据每个孩子的表现，预先给他们评分。
+ *
+ * 你需要按照以下要求，帮助老师给这些孩子分发糖果：
+ * 每个孩子至少分配到 1 个糖果。
+ * 评分更高的孩子必须比他两侧的邻位孩子获得更多的糖果。
+ * 那么这样下来，老师至少需要准备多少颗糖果呢？
+ * 示例 1：
+ * 输入：[1,0,2]
+ * 输出：5
+ * 解释：你可以分别给这三个孩子分发 2、1、2 颗糖果。
+ * 示例 2：
+ * 输入：[1,2,2]
+ * 输出：4
+ * 解释：你可以分别给这三个孩子分发 1、2、1 颗糖果。
+ *      第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+ */
+public class l017_candy_135 {
+    // 5 7 8 3 4 2 1
+    public int candy(int[] ratings) {
+        int[] left =  new int[ratings.length];
+        int[] right =  new int[ratings.length];
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+        for (int i = 1; i < right.length; i++) {
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1]+1;
+            }
+        }
+        //left:  1,2,3,1,2,1,1
+        //right: 1,1,2,1,3,2,1
+        int res = left[ratings.length-1];
+        for (int i = right.length -2; i >=0 ; i--) {
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1]+1;
+            }
+            res+=Math.max(left[i],right[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        l017_candy_135 demo = new l017_candy_135();
+        demo.candy(new int[]{5,7,8,3,4,2,1});
+    }
+}
