ae3f816778870680eb16d8361a794cf76cb5dbe6
jiatianyao <5lovezhm@163.com>
2021-07-13 00:08:29 +0800

    每日一题

--- src/main/java/每日一题/daily_0711_hIndex_274.java
+++ src/main/java/每日一题/daily_0711_hIndex_274.java
@@ -21,13 +21,19 @@ public class daily_0711_hIndex_274 {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int h = 0, i = citations.length - 1;
-        //如果当前 \text{H}H 指数为 hh 并且在遍历过程中找到当前值
-        // citations}[i] > h，则说明我们找到了一篇被引用了至少 h+1 次的论文，
+        //如果当前 H 指数为 h 并且在遍历过程中找到当前值
+        // citations[i] > h，则说明我们找到了一篇被引用了至少 h+1 次的论文，
         // 所以将现有的 h 值加 1
         while (i >= 0 && citations[i] > h) {
             h++;
             i--;
         }
         return h;
+    }
+
+
+    public static void main(String[] args) {
+        daily_0711_hIndex_274  demo = new daily_0711_hIndex_274();
+        demo.hIndex(new int[]{0,1,3,5,6});
     }
 }
