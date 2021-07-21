4a6113b6bb7f9ba5ca9668532da4b1982a361f85
jiatianyao <5lovezhm@163.com>
2021-07-21 20:35:26 +0800

    每日一题

---
+++ src/main/java/每日一题/daily_0718_groupAnagrams_10_02.java
@@ -0,0 +1,41 @@
+package 每日一题;
+
+import java.util.*;
+
+/**
+ * 面试题 10.02. 变位词组
+ * 编写一种方法，对字符串数组进行排序，将所有变位词组合在一起。变位词是指字母相同，但排列不同的字符串。
+ *
+ * 注意：本题相对原题稍作修改
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * 说明：
+ *
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ * https://leetcode-cn.com/problems/group-anagrams-lcci/
+ */
+public class daily_0718_groupAnagrams_10_02 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
